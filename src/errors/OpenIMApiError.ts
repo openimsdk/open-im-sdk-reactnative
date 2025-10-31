27fdf59d1b8eecedd95bbf18e7cc9b50d72df38b
OnYourMark-1995 <128215770+OnYourMark-1995@users.noreply.github.com>
2025-10-31 16:27:47 +0800

    feat: add operationID to OpenIMApiError and implement toJSON for serialization (#79)

--- src/errors/OpenIMApiError.ts
+++ src/errors/OpenIMApiError.ts
@@ -2,10 +2,23 @@ export class OpenIMApiError extends Error {
   name: string = 'OpenIMApiError';
   code: number;
   message: string;
+  operationID: string;
 
-  constructor(code: number, message: string) {
+  constructor(code: number, message: string, operationID: string) {
     super(message);
     this.code = code;
     this.message = message;
+    this.operationID = operationID;
+  }
+
+  // Error object's message and stack properties are non-enumerable by default.
+  // Override toJSON() to ensure the message property is properly serialized.
+  toJSON() {
+    return {
+      name: this.name,
+      operationID: this.operationID,
+      code: this.code,
+      message: this.message,
+    };
   }
 }
